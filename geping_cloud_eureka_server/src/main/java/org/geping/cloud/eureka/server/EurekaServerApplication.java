6bc44e5494e73272e5ff131e8299621f468c9ead
xiaoe <296556456@163.com>
2017-12-20 14:32:15 +0800

    first commit

---
+++ geping_cloud_eureka_server/src/main/java/org/geping/cloud/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package org.geping.cloud.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaServer
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
